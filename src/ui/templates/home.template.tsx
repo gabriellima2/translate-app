ede40056e14001203ff6cff47cbd1f1ee87ab4ef
Gabriel Lima <gabriel.rslima10@gmail.com>
2024-04-20 16:44:55 -0300

    feat: improve ux

--- src/ui/templates/home.template.tsx
+++ src/ui/templates/home.template.tsx
@@ -1,5 +1,5 @@
 import { debounce } from "@/helpers/debounce";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ArrowLeftRight } from "lucide-react-native";
 import { TextInput, TouchableOpacity, View } from "react-native";
 import colors from "tailwindcss/colors";
@@ -17,6 +17,7 @@ import type { LanguageCode } from "@/@types/language-code";
 type TranslateType = 'from' | 'to'
 
 export function HomeTemplate() {
+	const [value, setValue] = useState('')
 	const [translated, setTranslated] = useState('')
 	const languageRef = useRef<BottomSheetRef>(null)
 	const [language, setLanguage] = useState<{ from: LanguageCode, to: LanguageCode }>({
@@ -26,24 +27,24 @@ export function HomeTemplate() {
 	const [action, setAction] = useState<TranslateType>('from')
 
 	async function handleTranslate(v: string) {
-		debounce(async () => {
-			try {
-				if (!v) return setTranslated('')
-				const result = await translate(v, { from: language.from, to: language.to })
-				setTranslated(result)
-			} catch (err) {
-				console.log(err)
-			}
-		}, 250)
+		try {
+			if (!v) return setTranslated('')
+			const result = await translate(v, { from: language.from, to: language.to })
+			setTranslated(result)
+		} catch (err) {
+			console.log(err)
+		}
 	}
 
-	function handleListen() {
-		if (!translate) return
-		Speech.speak(translated, { language: languages.en.code })
+	function handleListen(sentence: string, language: string) {
+		if (!sentence) return
+		Speech.speak(sentence, { language })
 	}
 
 	function handleAlternate() {
 		setLanguage((prevState) => ({ to: prevState.from, from: prevState.to }))
+		setTranslated(value)
+		setValue(translated)
 	}
 
 	const languagesName = {
@@ -51,37 +52,51 @@ export function HomeTemplate() {
 		from: languages[language.from].name
 	}
 
+	useEffect(() => {
+		if (value) return
+		setTranslated('')
+	}, [value])
+
+	useEffect(() => {
+		if (!value) return
+		handleTranslate(value)
+	}, [language.to])
+
 	return (
 		<View className="flex-1 p-4 gap-y-4">
 			<View className="bg-neutral-800 rounded-3xl min-h-[124px] p-4 justify-between">
 				<Typography.Title className="text-sm">{languagesName.from}</Typography.Title>
 				<TextInput
 					placeholder="Type something..."
-					onChangeText={handleTranslate}
+					value={value}
+					onChangeText={(t) => {
+						setValue(t)
+						debounce(async () => await handleTranslate(t), 250)
+					}}
 					placeholderTextColor={colors.neutral[400]}
 					className="text-white text-lg font-subtitle my-4"
 					multiline
 				/>
 				<AudioButton
 					disabled={!translate}
-					onPress={handleListen}
+					onPress={() => handleListen(value, language.from)}
 					accessibilityLabel="Listen to pronunciation"
 					className="self-end"
 				/>
 			</View>
 			<View className="bg-neutral-800 rounded-3xl min-h-[124px] p-4 justify-between">
-				<Typography.Title className="text-sm">{languagesName.to}</Typography.Title>
+				<Typography.Title className="text-sm text-emerald-300">{languagesName.to}</Typography.Title>
 				<TextInput
 					placeholder="Translation..."
 					readOnly
 					value={translated}
 					placeholderTextColor={colors.neutral[400]}
-					className="text-white text-lg font-subtitle my-4"
+					className="text-lg font-subtitle my-4 text-emerald-300"
 					multiline
 				/>
 				<AudioButton
 					disabled={!translate}
-					onPress={handleListen}
+					onPress={() => handleListen(translated, language.to)}
 					accessibilityLabel="Listen to pronunciation"
 					className="self-end"
 				/>
@@ -89,7 +104,7 @@ export function HomeTemplate() {
 			<View className="flex-row w-full">
 				<LanguageSelect.Trigger
 					activeLanguage={languagesName.from}
-						handleExpand={() => {
+					handleExpand={() => {
 						setAction('from')
 						languageRef?.current?.expand()
 					}}
@@ -113,8 +128,18 @@ export function HomeTemplate() {
 				ref={languageRef}
 				language={language[action]}
 				onLanguageChange={(v) => {
-					if (action === 'from') return setLanguage((prevState) => ({ ...prevState, from: v }))
-					setLanguage((prevState) => ({ ...prevState, to: v }))
+					if (v === language.from || v === language.to) {
+						languageRef?.current?.close()
+						return handleAlternate()
+					}
+					if (action === 'from') {
+						setValue('')
+						setLanguage((prevState) => ({ ...prevState, from: v }))
+					}
+					if (action === 'to') {
+						setLanguage((prevState) => ({ ...prevState, to: v }))
+					}
+					languageRef?.current?.close()
 				}}
 			/>
 		</View>
